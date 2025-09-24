7dd8b67ec3f455738b6507767b3dba486fb63678
Colas NAUDI <colasnaudi@gmail.com>
2025-09-24 14:33:13 +0200

    Add dialog

---
+++ src/dialog/useDialog.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const possibleStates = Object.freeze({
+    EN_COURS :{
+        icon: "in_progress"
+    },
+    TERMINE: {
+        icon: "done"
+    }
+});
+
+export function useDialog() {
+    const listItem = useState([]);
+
+    // Fonction pour ajouter une tâche
+
+
+    // Fonction pour supprimer une tâche
+
+    
+    // Fonction pour changer l'état d'une tâche
+};
